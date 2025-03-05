e28902c87eed77e16b03da808b5df260838aae1b
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2025-03-05 16:47:09 +0600

    Add files via upload

---
+++ client/src/socket.js
@@ -0,0 +1,26 @@
+import io from 'socket.io-client';
+
+const SOCKET_URL = 'http://localhost:3001';
+const socket = io(SOCKET_URL, {
+  transports: ['websocket', 'polling'],
+  autoConnect: true,
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  timeout: 20000,
+});
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('connect_error', (error) => {
+  console.error('Connection error:', error);
+});
+
+socket.on('disconnect', (reason) => {
+  console.log('Disconnected:', reason);
+});
+
+export default socket;
